1e0d752d28dbc025606ebb254b40c38037972038
Volodymyr Kostenko <v.kostenko.job@gmail.com>
2023-07-26 19:39:55 +0300

    Add test Get contact by existing id

--- src/test/java/api/easyPay_Api/tests/Contacts.java
+++ src/test/java/api/easyPay_Api/tests/Contacts.java
@@ -66,5 +66,50 @@ public class Contacts {
                 .then().log().all().statusCode(200);
     }
 
+    @Test
+    @DisplayName("Get contact by existing id")
+    @Owner("Volodymyr Kostenko")
+    public void getContactById(){
+
+        // Get createApp -----------
+        Response createAppResponse = given()
+                .when().post("https://apistage.easypay.ua/api/system/createApp")
+                .then().statusCode(200)
+                .extract().response();
+
+        String appId = createAppResponse.jsonPath().getString("appId");
+        String pageId = createAppResponse.jsonPath().getString("pageId");
+
+        // Get token -----------
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addHeader("accept", "application/json")
+                .addHeader("Locale", "UA")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("PartnerKey", "easypay-v2")
+                .addHeader("AppId", appId)
+                .build();
+
+        AuthUser authUser = new AuthUser("380958872559", "1234567Qq");
+
+        String token = given().spec(requestSpecification).body(authUser)
+                .when().post("https://authstage.easypay.ua/api/auth/desktop")
+                .then().extract().jsonPath().getString("data.access_token");
+
+        // Test -----------
+        RequestSpecification specification = new RequestSpecBuilder()
+                .addHeader("Accept", "application/json")
+                .addHeader("PartnerKey", "easypay-v2")
+                .addHeader("locale", "UA")
+                .addHeader("koatuu", "8000000000")
+                .addHeader("AppId", appId)
+                .addHeader("PageId", pageId)
+                .addHeader("Authorization", "Bearer " + token)
+                .build();
+
+        given().spec(specification).log().all()
+                .when().get("https://apistage.easypay.ua/api/contacts/get/13")
+                .then().log().all().statusCode(200);
+    }
+
 
 }
